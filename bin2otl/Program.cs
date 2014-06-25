5596fe8c7f5696fb360d91771aeeaea53af37835
Evgeniy Plyusnin <eninbox@gmail.com>
2014-06-25 10:54:51 +0600

    Добавлена добивка "двух строчек FF" в конце файла, чтобы работало

--- bin2otl/Program.cs
+++ bin2otl/Program.cs
@@ -2,18 +2,15 @@
 using System.Collections.Generic;
 using System.IO;
 using dat2otl.Properties;
-using System.Linq;
 
 namespace bin2otl
 {
-    class Program
+    internal class Program
     {
-        public const int PageSize = 0x10000;    // 65536
+        public const int PageSize = 0x10000; // 65536
 
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            const int minimumFileLength = 65*1024;
-
             string inputFileName = null;
             if (args.Length >= 1) inputFileName = args[0];
             else
@@ -40,14 +37,15 @@ namespace bin2otl
                         outputStream.Write(message, 0, message.Length);
                     else
                     {
-                        StuffPage(outputStream);
+                        Stuff(outputStream);
                         pageHeaderStream.Seek(0, SeekOrigin.Begin);
                         pageHeaderStream.CopyTo(outputStream);
                         outputStream.Write(message, 0, message.Length);
                     }
                 }
 
-                StuffPage(outputStream);
+                Stuff(outputStream);
+                Stuff(outputStream, 32);
             }
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Готово");
@@ -55,7 +53,13 @@ namespace bin2otl
             Console.ReadLine();
         }
 
-        private static void StuffPage(Stream stuffingStream) { while (stuffingStream.Length % PageSize != 0) stuffingStream.WriteByte(0xff); }
+        private static void Stuff(Stream stuffingStream, int stuffLength)
+        {
+            for (int i = 0; i < stuffLength; i++)
+                stuffingStream.WriteByte(0xff);
+        }
+
+        private static void Stuff(Stream stuffingStream) { while (stuffingStream.Length % PageSize != 0) stuffingStream.WriteByte(0xff); }
 
         public static Byte[] ReadMessage(Stream InputStream)
         {
@@ -65,7 +69,7 @@ namespace bin2otl
             {
                 b = InputStream.ReadByte();
                 if (b == -1) break;
-                else res.Add((byte)b);
+                res.Add((byte)b);
             } while (b != 0xFF);
             return res.ToArray();
         }
